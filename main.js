b982e1cde8c397fe8532216e6d3404455be902bb
brunogrossi4 <105368343+brunogrossi4@users.noreply.github.com>
2024-03-21 08:32:17 -0300

    vvbv

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ for(let i=0; i <botoes.length; i++){
       }
 
 }
-contadores[0].textContent = "Contagem regressiva";
+contadores[0].textContent = tempoObjetivo1;
 const tempoObjetivo1 = new Date ("2024-03-21");
 
 contadores[0].textcontent = tempoObjetivo1;
